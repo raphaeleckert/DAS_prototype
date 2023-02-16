1204a52a54846ee61e70390e4fd3446c68b3329e
reckert <reckert@hs-heilbronn.de>
2023-02-16 12:21:24 +0100

    templates + css

--- das/main.go
+++ das/main.go
@@ -13,13 +13,18 @@ func homePage(writer http.ResponseWriter, request *http.Request) {
 }
 
 func login(writer http.ResponseWriter, request *http.Request) {
-	t, _ := template.ParseFiles("/templates/registration/login.html")
-	t.Execute(writer, nil)
+	t, _ := template.ParseFiles(
+		"C:/Users/rapha/Desktop/Bachelorarbeit/DAS_prototype/das/templates/base.html",
+		"C:/Users/rapha/Desktop/Bachelorarbeit/DAS_prototype/das/templates/registration/login.html")
+	t.ExecuteTemplate(writer, "base", nil)
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/login/", login)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
