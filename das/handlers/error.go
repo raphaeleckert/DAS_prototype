59e5d2375cdf92f531808554328b0922c60e37b0
reckert <reckert@hs-heilbronn.de>
2023-03-20 17:50:23 +0100

    error msg

--- das/handlers/error.go
+++ das/handlers/error.go
@@ -4,10 +4,9 @@ import (
 	"dasagilestudieren/utils"
 
 	"net/http"
-	"html/template"
 )
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request) {
-	
-utils.ShowErrorMsg()
+
+	utils.ShowErrorMsg("test", w)
 }
